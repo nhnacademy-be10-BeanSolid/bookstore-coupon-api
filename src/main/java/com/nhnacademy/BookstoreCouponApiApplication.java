38bfd6a90b6e909e5c8fd292f3894423dac393ff
WooMin-Kim <kkurew12@gmail.com>
2025-06-10 09:34:40 +0900

    feat: [초기 설정] 쿠폰 서비스 기본 구조 및 Spring Boot 설정

---
+++ src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreCouponApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreCouponApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/BookstoreCouponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreCouponApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
