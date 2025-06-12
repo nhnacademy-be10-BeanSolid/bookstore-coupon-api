d85c5aa2935c5fd688152fa7a57bd58cbc1e3c82
WooMin-Kim <kkurew12@gmail.com>
2025-06-12 17:05:44 +0900

    chore: 애플리케이션 메인 클래스 설정 업데이트

--- src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
+++ src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
@@ -2,12 +2,13 @@ package com.nhnacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BookstoreCouponApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookstoreCouponApiApplication.class, args);
     }
-
 }
