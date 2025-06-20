568599345e2332ced5cf094426686da238081b3e
WooMin-Kim <kkurew12@gmail.com>
2025-06-20 14:37:41 +0900

    refactor: CouponUser 구조 및 개선

---
+++ src/main/java/com/nhnacademy/domain/CouponUserId.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.domain;
+
+import lombok.*;
+import java.io.Serializable;
+
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CouponUserId implements Serializable {
+    private Long couponId;
+    private String userId;
+}
