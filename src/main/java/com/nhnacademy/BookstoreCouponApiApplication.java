f6ba4072f8809faecba39ab833fb2507cc835ed0
WooMin-Kim <kkurew12@gmail.com>
2025-06-20 14:38:21 +0900

    feat: Spring Boot 애플리케이션 초기 설정

--- src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
+++ src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
@@ -2,10 +2,12 @@ package com.nhnacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EntityScan(basePackages = {"com.nhnacademy.domain"})
 public class BookstoreCouponApiApplication {
 
     public static void main(String[] args) {
