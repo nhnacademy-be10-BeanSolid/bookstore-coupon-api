2217889a69a7c0f1414afea2522f8880c49884c5
NaIlHwan <nlhjobs01@naver.com>
2025-06-25 15:07:13 +0900

    feat: Eureka Client 설정 추가

--- src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
+++ src/main/java/com/nhnacademy/BookstoreCouponApiApplication.java
@@ -3,11 +3,13 @@ package com.nhnacademy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EntityScan(basePackages = {"com.nhnacademy.domain"})
+@EnableDiscoveryClient
 public class BookstoreCouponApiApplication {
 
     public static void main(String[] args) {
