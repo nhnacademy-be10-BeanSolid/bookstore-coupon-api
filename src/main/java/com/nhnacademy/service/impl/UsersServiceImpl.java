8ebd2f8ee05a46e40f1d4b3290d33f68116b43e8
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-11 12:49:26 +0900

    feat: 쿠폰 발급을 위해 불필요한 파일 삭제

--- src/main/java/com/nhnacademy/service/impl/UsersServiceImpl.java
+++ src/main/java/com/nhnacademy/service/impl/UsersServiceImpl.java
@@ -1,7 +1,8 @@
 package com.nhnacademy.service.impl;
 
-import com.nhnacademy.domain.Users;
+
 import com.nhnacademy.repository.UsersRepository;
+
 import com.nhnacademy.service.CouponService;
 import com.nhnacademy.service.UsersService;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +16,11 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class UsersServiceImpl implements UsersService {
     private final UsersRepository usersRepository;
-    private final CouponService couponService;
 
     @Override
     public List<String> getUserIdsByBirthMonth(int month) {
         return usersRepository.findUserIdsByBirthMonth(month);
     }
 
-    public Users registerUser(Users users) {
-        Users savedUser = usersRepository.save(users);
-
-        try {
-            couponService.issueWelcomeCoupon(savedUser.getUserId());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return savedUser;
-    }
+    
 }
