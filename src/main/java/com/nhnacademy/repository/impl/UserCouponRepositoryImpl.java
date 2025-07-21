05dae33ba8151732f4c0201a7cb8763af025a935
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-21 14:40:26 +0900

    feat: issuedAt 추가

--- src/main/java/com/nhnacademy/repository/impl/UserCouponRepositoryImpl.java
+++ src/main/java/com/nhnacademy/repository/impl/UserCouponRepositoryImpl.java
@@ -28,7 +28,6 @@ public class UserCouponRepositoryImpl implements UserCouponRepositoryCustom {
                 .selectFrom(usedCoupon)
                 .where(usedCoupon.userNo.eq(userNo)
                         .and(usedCoupon.status.eq(UserCouponStatus.ACTIVE))
-                        .and(usedCoupon.issuedAt.between(startDate, endDate))
                         .and(usedCoupon.expiredAt.after(LocalDateTime.now())))
                 .fetch();
     }
