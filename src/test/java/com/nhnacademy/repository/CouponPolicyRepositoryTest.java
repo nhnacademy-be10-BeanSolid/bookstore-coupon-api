383662bc5a2f2c31afa09b547ed0aab66c34364b
JYCuzz <hanafos944@naver.com>
2025-07-22 14:48:09 +0900

    feat : 포트 10353 에서 10352 로 수정

--- src/test/java/com/nhnacademy/repository/CouponPolicyRepositoryTest.java
+++ src/test/java/com/nhnacademy/repository/CouponPolicyRepositoryTest.java
@@ -8,14 +8,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class CouponPolicyRepositoryTest {
 
     @Autowired
